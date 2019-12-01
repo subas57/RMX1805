d4a37cf791405d9585a392b4ce649523f7ff72a4
sb6596 <shubhamprince111@gmail.com>
2019-12-01 08:38:42 +0000

    RMX1805: Define BLUETOOTH_QTI_SW

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,6 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+#define BLUETOOTH_QTI_SW  TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16

fbb8752327d42e7a8914a0923a06002345013af3
luk1337 <priv.luk@gmail.com>
2021-07-27 20:49:33 +0530

    RMX1805: Include QSSI sepolicy
    * Now that device/qcom/sepolicy is non-vendor only
      we are able to do that.
    
    Change-Id: I4c83a8e30cfa11e45be2f06eb7f444de7f68d219
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,8 +111,7 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+include device/qcom/sepolicy/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble

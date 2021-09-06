5a4d85a5b1424b5b60474f51e972666493242d25
abhinavftp98 <abhishekftp99@gmailcom>
2021-09-06 15:56:27 +0530

    RMX1805 : Revert RealmeDirac

--- device.mk
+++ device.mk
@@ -47,9 +47,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml
 
-# Dirac
-$(call inherit-product, packages/apps/RealmeDirac/dirac.mk)
-
 # Doze
 PRODUCT_PACKAGES += \
     RealmeParts
@@ -62,10 +59,6 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
-
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \

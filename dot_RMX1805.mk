bf46f1d5097382b9fa8d97ea984fb9b6b01616e3
Subas <77564932+subas57@users.noreply.github.com>
2021-09-08 21:50:09 +0530

    Update and rename cherish_RMX1805.mk to dot_RMX1805.mk

--- dot_RMX1805.mk
+++ dot_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Cherish stuff
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := cherish_RMX1805
+PRODUCT_NAME := dot_RMX1805
 PRODUCT_MODEL := Realme 2/C1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
@@ -24,11 +24,11 @@ TARGET_VENDOR_PRODUCT_NAME := RMX1805
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
-TARGET_GAPPS_ARCH := ARM64
+#TARGET_GAPPS_ARCH := ARM64
 TARGET_BOOT_ANIMATION_RES := 720
 
 #CHERISH STAFF
-CHERISH_BUILD_TYPE := OFFICIAL
+#CHERISH_BUILD_TYPE := OFFICIAL
 WITH_GMS := true
 #USE_LAWNCHAIR :=true
 TARGET_INCLUDE_STOCK_ARCORE := true
@@ -38,5 +38,4 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cherish.maintainer=ABHINAV™
+
